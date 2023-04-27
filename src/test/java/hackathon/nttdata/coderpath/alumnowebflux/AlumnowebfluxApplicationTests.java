622b22445dc7cf3bf6f1825ee4cb4e3846726780
Joffre Hermosilla Salas <joffre.hermosilla@gmail.com>
2023-04-27 15:44:50 -0500

    chore[1] primera configuracion

---
+++ src/test/java/hackathon/nttdata/coderpath/alumnowebflux/AlumnowebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.nttdata.coderpath.alumnowebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumnowebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

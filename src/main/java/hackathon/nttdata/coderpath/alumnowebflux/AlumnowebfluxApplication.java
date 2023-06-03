7fa1e7f7d1e1e900978016140a3e169475d87f84
carlo delgado <cdmarallano23@gmail.com>
2023-06-03 02:44:27 -0500

    chore[12]: adicionando la observable

--- src/main/java/hackathon/nttdata/coderpath/alumnowebflux/AlumnowebfluxApplication.java
+++ src/main/java/hackathon/nttdata/coderpath/alumnowebflux/AlumnowebfluxApplication.java
@@ -1,16 +1,28 @@
 package hackathon.nttdata.coderpath.alumnowebflux;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import reactor.core.publisher.Flux;
+
 
 @EnableEurekaClient
 @SpringBootApplication
-public class AlumnowebfluxApplication {
+public class AlumnowebfluxApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlumnowebfluxApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		   Flux<String> nombres = Flux.just("Joffre, Allison, Mila, Diego")
+		   			.doOnNext(elemento -> System.out.println(elemento));
+	   		
+		   nombres.subscribe();
+	}   
+	
 
 }

c067ad8524d0b25860518105e6d51bf73058846c
carlo delgado <cdmarallano23@gmail.com>
2023-06-04 01:54:07 -0500

    chore[13]: adicionando el ovservable emulnado errores de vacio

--- src/main/java/hackathon/nttdata/coderpath/alumnowebflux/AlumnowebfluxApplication.java
+++ src/main/java/hackathon/nttdata/coderpath/alumnowebflux/AlumnowebfluxApplication.java
@@ -1,5 +1,7 @@
 package hackathon.nttdata.coderpath.alumnowebflux;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,18 +13,28 @@ import reactor.core.publisher.Flux;
 @EnableEurekaClient
 @SpringBootApplication
 public class AlumnowebfluxApplication implements CommandLineRunner{
-
+	
+	private static final Logger log = LoggerFactory.getLogger(AlumnowebfluxApplication.class);  
 	public static void main(String[] args) {
 		SpringApplication.run(AlumnowebfluxApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		   Flux<String> nombres = Flux.just("Joffre, Allison, Mila, Diego")
-		   			.doOnNext(elemento -> System.out.println(elemento));
+		
+		//*Flux<String> nombres = Flux.just("Joffre", "tangiro", "Mila", "Diego", "Joffre", "tangiro", "Mila", "Diego") //*
+		//*  .doOnNext(elemento -> System.out.println(elemento));//*
 	   		
-		   nombres.subscribe();
-	}   
+		//*nombres.subscribe(log::info);//*
 	
-
+		Flux<String> nombres = Flux.just("Joffre", "tangiro", "Mila", "Diego", "Joffre", "tangiro", "Mila", "Diego")
+				.doOnNext(err -> {
+					if(err.isEmpty()) {
+						throw new RuntimeException("nombres vacios");}
+					{System.out.println(err);}
+					});
+		nombres.subscribe(err -> log.info(err));
+				
+		
+	}   
 }

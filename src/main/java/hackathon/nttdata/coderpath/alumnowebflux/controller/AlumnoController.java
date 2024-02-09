0c234b417c368bb2fe0cf05d94995af14f103ae5
joffre <joffre.hermosilla@gmail.com>
2024-02-09 10:40:29 -0500

    configuracion netty para open jdk 17

--- src/main/java/hackathon/nttdata/coderpath/alumnowebflux/controller/AlumnoController.java
+++ src/main/java/hackathon/nttdata/coderpath/alumnowebflux/controller/AlumnoController.java
@@ -60,82 +60,73 @@ public class AlumnoController {
 	final String urlServer = "http://localhost:8090/api/alumnowebflux";
 
 	//// validadores de conexion webclient
-	@GetMapping("/{param}")
-	public Mono<ResponseEntity<Mono<String>>> testGet(@PathVariable String param) {
-		final long dateStarted = System.currentTimeMillis();
+	/*
+	 * @GetMapping("/{param}") public Mono<ResponseEntity<Mono<String>>>
+	 * testGet(@PathVariable String param) { final long dateStarted =
+	 * System.currentTimeMillis();
+	 * 
+	 * WebClient webClient = WebClient.create(urlServer + "/server/");
+	 * Mono<ClientResponse> respuesta = webClient.get().uri("?queryParam={name}",
+	 * param).exchange(); Mono<ClientResponse> respuesta1 = webClient.get()
+	 * .uri("?queryParam={name}", "SPEED".equals(param) ? "SPEED" :
+	 * "STOP").exchange();
+	 * 
+	 * Mono<ResponseEntity<Mono<String>>> f1 = Mono.zip(respuesta, respuesta1).map(t
+	 * -> { if (!t.getT1().statusCode().is2xxSuccessful()) { return
+	 * ResponseEntity.status(t.getT1().statusCode()).body(t.getT1().bodyToMono(
+	 * String.class)); } if (!t.getT2().statusCode().is2xxSuccessful()) { return
+	 * ResponseEntity.status(t.getT2().statusCode()).body(t.getT2().bodyToMono(
+	 * String.class)); } return ResponseEntity.ok().body(Mono.just(
+	 * "All OK. Seconds elapsed: " + (((double) (System.currentTimeMillis() -
+	 * dateStarted) / 1000)))); }); return f1; }
+	 */
 
-		WebClient webClient = WebClient.create(urlServer + "/server/");
-		Mono<ClientResponse> respuesta = webClient.get().uri("?queryParam={name}", param).exchange();
-		Mono<ClientResponse> respuesta1 = webClient.get()
-				.uri("?queryParam={name}", "SPEED".equals(param) ? "SPEED" : "STOP").exchange();
+	/*
+	 * @PostMapping("") public Mono<String> testURLs(@RequestBody Map<String,
+	 * String> body, @RequestParam(required = false) String url) {
+	 * 
+	 * log.debug("Client: in testURLs"); WebClient.Builder builder =
+	 * WebClient.builder().baseUrl(urlServer).defaultHeader(HttpHeaders.
+	 * CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE); if (body != null &&
+	 * body.size() > 0) { for (Map.Entry<String, String> map : body.entrySet()) {
+	 * builder.defaultHeader(map.getKey(), map.getValue()); } } WebClient webClient
+	 * = builder.build(); String urlFinal; if (url == null) urlFinal =
+	 * "/server/post"; else urlFinal = "/server/" + url;
+	 * 
+	 * Mono<String> respuesta1 =
+	 * webClient.post().uri(urlFinal).body(BodyInserters.fromObject(body)).exchange(
+	 * ) .flatMap(x -> { if (!x.statusCode().is2xxSuccessful()) return
+	 * Mono.just("LLamada a " + urlServer + urlFinal + " Error 4xx: " +
+	 * x.statusCode() + "\n"); return x.bodyToMono(String.class); }); return
+	 * respuesta1; }
+	 */
 
-		Mono<ResponseEntity<Mono<String>>> f1 = Mono.zip(respuesta, respuesta1).map(t -> {
-			if (!t.getT1().statusCode().is2xxSuccessful()) {
-				return ResponseEntity.status(t.getT1().statusCode()).body(t.getT1().bodyToMono(String.class));
-			}
-			if (!t.getT2().statusCode().is2xxSuccessful()) {
-				return ResponseEntity.status(t.getT2().statusCode()).body(t.getT2().bodyToMono(String.class));
-			}
-			return ResponseEntity.ok().body(Mono.just(
-					"All OK. Seconds elapsed: " + (((double) (System.currentTimeMillis() - dateStarted) / 1000))));
-		});
-		return f1;
-	}
-
-	@PostMapping("")
-	public Mono<String> testURLs(@RequestBody Map<String, String> body, @RequestParam(required = false) String url) {
-
-		log.debug("Client: in testURLs");
-		WebClient.Builder builder = WebClient.builder().baseUrl(urlServer).defaultHeader(HttpHeaders.CONTENT_TYPE,
-				MediaType.APPLICATION_JSON_VALUE);
-		if (body != null && body.size() > 0) {
-			for (Map.Entry<String, String> map : body.entrySet()) {
-				builder.defaultHeader(map.getKey(), map.getValue());
-			}
-		}
-		WebClient webClient = builder.build();
-		String urlFinal;
-		if (url == null)
-			urlFinal = "/server/post";
-		else
-			urlFinal = "/server/" + url;
-
-		Mono<String> respuesta1 = webClient.post().uri(urlFinal).body(BodyInserters.fromObject(body)).exchange()
-				.flatMap(x -> {
-					if (!x.statusCode().is2xxSuccessful())
-						return Mono.just("LLamada a " + urlServer + urlFinal + " Error 4xx: " + x.statusCode() + "\n");
-					return x.bodyToMono(String.class);
-				});
-		return respuesta1;
-	}
-
-	@PostMapping("post")
-	public ResponseEntity<String> postExample(@RequestBody Map<String, String> body, ServerHttpRequest request) {
-		String s = "the server said: " + body + "\n";
-		for (Map.Entry<String, List<String>> map : request.getHeaders().entrySet()) {
-			s += "Headers: " + map.getKey() + ":" + map.getValue().get(0) + "\n";
-		}
-		return ResponseEntity.ok().body(s);
-	}
-
+	/*
+	 * @PostMapping("post") public ResponseEntity<String> postExample(@RequestBody
+	 * Map<String, String> body, ServerHttpRequest request) { String s =
+	 * "the server said: " + body + "\n"; for (Map.Entry<String, List<String>> map :
+	 * request.getHeaders().entrySet()) { s += "Headers: " + map.getKey() + ":" +
+	 * map.getValue().get(0) + "\n"; } return ResponseEntity.ok().body(s); }
+	 */
 	//// prueba webclient
 
-	@PostMapping("/producer/{topic}")
-	public ResponseEntity<Mono<?>> publishMessage(@PathVariable String topic, @Valid @RequestBody String message) {
-		Mono.just(producer).doOnNext(t -> {
-
-			t.publishMessage(topic, message);
-
-		}).onErrorReturn(producer).onErrorResume(e -> Mono.just(producer))
-				.onErrorMap(f -> new InterruptedException(f.getMessage())).subscribe(x -> log.info(x.toString()));
-
-		Mono<String> pAsset = Mono.just(message);
-
-		if (pAsset != null) {
-			return new ResponseEntity<>(pAsset, HttpStatus.CREATED);
-		}
-		return new ResponseEntity<>(Mono.just(new Alumno()), HttpStatus.I_AM_A_TEAPOT);
-	}
+	/*
+	 * @PostMapping("/producer/{topic}") public ResponseEntity<Mono<?>>
+	 * publishMessage(@PathVariable String topic, @Valid @RequestBody String
+	 * message) { Mono.just(producer).doOnNext(t -> {
+	 * 
+	 * t.publishMessage(topic, message);
+	 * 
+	 * }).onErrorReturn(producer).onErrorResume(e -> Mono.just(producer))
+	 * .onErrorMap(f -> new InterruptedException(f.getMessage())).subscribe(x ->
+	 * log.info(x.toString()));
+	 * 
+	 * Mono<String> pAsset = Mono.just(message);
+	 * 
+	 * if (pAsset != null) { return new ResponseEntity<>(pAsset,
+	 * HttpStatus.CREATED); } return new ResponseEntity<>(Mono.just(new Alumno()),
+	 * HttpStatus.I_AM_A_TEAPOT); }
+	 */
 
 	@GetMapping("/balanceador-test")
 	public ResponseEntity<?> balanceadorTest() {
